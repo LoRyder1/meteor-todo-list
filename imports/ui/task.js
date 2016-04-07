44f2aac402783501f3a250305e316b5f8a052a19
Lovinder Pnag <lspnag@gmail.com>
2016-04-07 19:20:04 -0400

    delete only public items

--- imports/ui/task.js
+++ imports/ui/task.js
@@ -5,12 +5,22 @@ import { Tasks } from '../api/tasks.js';
 
 import './task.html';
 
+Template.task.helpers({
+  isOwner() {
+    return this.owner === Meteor.userId();
+  },
+});
+
 Template.task.events({
   'click .toggle-checked'() {
     // Set the checked property to the opposite of its current value
     Meteor.call('tasks.setChecked', this._id, !this.checked);
   },
   'click .delete'() {
+    // Meteor call is an implementation of Optimistic UI
     Meteor.call('tasks.remove', this._id);
+  },
+  'click .toggle-private'() {
+    Meteor.call('tasks.setPrivate', this._id, !this.private);
   },
 });
